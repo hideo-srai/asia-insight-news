af7d6b63889f663143b65dbd6814be6f8aeb8a1d
Hideo Shirai <hideo.srai@gmail.com>
2015-09-01 23:50:13 +0300

    Assets

--- public/assets/ckeditor/plugins/preview/preview-577809bd30e0d996a3fa213abfb23947.js
+++
@@ -1,7 +0,0 @@
-// Angular Rails Template
-// source: /home/reiro/.rvm/gems/ruby-2.1.5@mni-asia-insight/gems/ckeditor-4.1.1/vendor/assets/javascripts/ckeditor/plugins/preview/preview.html
-
-angular.module("templates").run(["$templateCache", function($templateCache) {
-  $templateCache.put("ckeditor/plugins/preview/preview.html", "<script>\n\n// Prevent from DOM clobbering.\nif ( typeof window.opener._cke_htmlToLoad == 'string' ) {\n	var doc = document;\n	doc.open();\n	doc.write( window.opener._cke_htmlToLoad );\n	doc.close();\n\n	delete window.opener._cke_htmlToLoad;\n}\n\n</script>")
-}]);
-
