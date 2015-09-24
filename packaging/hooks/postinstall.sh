202a5782d49ef4ba25ff230e9fa760578b64893a
Hideo Shirai <hideo.srai@gmail.com>
2015-09-25 00:29:04 +0300

    Remove migration from post-install

--- packaging/hooks/postinstall.sh
+++ packaging/hooks/postinstall.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 set -e
-echo 'Asia Insight installed!'
-echo 'Migrating the database...'
-asia-insight config:set RAILS_ENV=production
-asia-insight run rake db:migrate
-echo 'Database successfully migrated!'
 touch tmp/restart.txt
+echo 'Asia Insight installed!'
 exit 0
